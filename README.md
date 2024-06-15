afd52ce4783acae334688eac0d6cdc7d7e67caf6
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-06-15 20:24:27 +0530

    Used Auth in homepage

--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 -[x] tidy up the build process
 -[x] Actually set up a database
 -[x] Attach db to UI
--[] make auth
+-[x] make auth (w/ clerk)
+-[] "taint" (server-only)
+-[] Use Next/Image Component
 -[] add image upload
 -[] error management(w/ senry)
 -[] routing/image page(paraller route)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import { headers } from "next/headers";
-import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 // Define the mock URLs array
@@ -8,23 +7,35 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic"
 
 
-// HomePage component
-export default async function HomePage() {
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy:(model,{desc})=>desc(model.id)
   });
 
-  return (
-    <div className="flex flex-wrap">
-    
-
-    {[...images, ...images, ...images].map((image, index) => (
-      <div key={image.id + "-" + index} className="flex w-48 flex-col p-4">
-        <img src={image.url} alt={`Mock Image ${index + 1}`} />
-        <div>{image.name}</div>
-      </div>
-    ))}
-
+  return(
+    <div className="flex flex-wrap gap-4">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col p-4">
+            <img src={image.url} alt={`Mock Image ${index + 1}`} />
+            <div>{image.name}</div>
+          </div>
+          
+        ))}
     </div>
+    )
+}
+
+// HomePage component
+export default async function HomePage() {
+  return (
+    <main className="flex flex-wrap">
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
+
+    </main>
   );
 }
