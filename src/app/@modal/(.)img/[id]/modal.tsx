b30e797df2ca6328aa4d99024ff816afcdd0d4e5
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-06-19 08:50:05 +0530

    more cleanup in modal

--- src/app/@modal/(.)img/[id]/modal.tsx
+++ src/app/@modal/(.)img/[id]/modal.tsx
@@ -22,10 +22,11 @@ export function Modal({ children }: { children: React.ReactNode }) {
     <div className="modal-backdrop">
       <dialog 
         ref={dialogRef} 
-        className="m-0 h-screen w-screen bg-zinc-900/50" 
+        className="m-0 h-screen w-screen bg-zinc-900/50 text-white" 
         onClose={onDismiss}>
           {children}
-          <button onClick={onDismiss} className="close-button" />
+          
+          {/* <button onClick={onDismiss} className="close-button" />*/}
       </dialog>
     </div>,
     document.getElementById('modal-root')!
--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -2,5 +2,17 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props : {id: number}) {
   const image = await getImage(props.id)
-  return <img src={image.url} className="w-96"/>;
+  return ( 
+    <div className = "flex h-full w-full min-w-0">
+      
+      <div className = "flex flex-shrink justify-center items-center">
+        <img src={image.url} className="flex-shrink object-contain"/>
+      </div>
+      
+      <div className="flex w-48 flex-shrink-0 flex-col border-l-2 ">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    
+    </div>  
+  );
 }
